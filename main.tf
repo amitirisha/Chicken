0f41461395244a6b4099b16c85cc68c66cc0a662
Amivn <xcz@gmail.com>
2025-07-25 10:40:24 +0530

    second code

--- main.tf
+++ main.tf
@@ -3,3 +3,8 @@ resource "azurerm_resource_group" "rg" {
     name= "ragini-rg"
     location = "westus"
 }
+
+resource "azurerm_resource_group" "rg1" {
+    name= "vicky-rg"
+    location = "westus"
+}
