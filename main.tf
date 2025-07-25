c9185dab8097cce87060222e5592552eecd7458e
amitirisha <118282287+amitirisha@users.noreply.github.com>
2025-07-25 11:07:11 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -12,3 +12,7 @@ resource "azurerm_resource_group" "rg2" {
     name= "amit-rg"
     location = "westus"
 }
+resource "azurerm_resource_group" "rg3" {
+    name= "devops-rg"
+    location = "westus"
+}
